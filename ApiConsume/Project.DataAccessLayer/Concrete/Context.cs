2588d342d52b55c53aad6cdb527cd84a04f9b01e
Shenol <shenol88@hotmail.com>
2024-07-23 11:33:54 +0200

    add identity package

--- ApiConsume/Project.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/Project.DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Project.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace Project.DataAccessLayer.Concrete
 {
-    public class Context: DbContext
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;initial catalog=ReservationProjectAPI:integrated security=true");
         }
         public DbSet<Room> Rooms { get; set; }
-        public DbSet<Service> Services{ get; set; }
+        public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staff { get; set; }
         public DbSet<MailSubscribe> MailSubscribes { get; set; }
         public DbSet<Testimonial> Testimonial { get; set; }
