af3e33223e3e536fd8a72f61c4b515bdbe5e20ef
Shenol <shenol88@hotmail.com>
2024-07-28 22:10:47 +0200

    room section on homepage

--- Frontend/Project.WebUI/Helpers/Mapper/AutoMapperConfig.cs
+++ Frontend/Project.WebUI/Helpers/Mapper/AutoMapperConfig.cs
@@ -2,6 +2,7 @@
 using Project.EntityLayer.Concrete;
 using Project.WebUI.Dtos.AboutUsDto;
 using Project.WebUI.Dtos.AppUserDto;
+using Project.WebUI.Dtos.RoomDto;
 using Project.WebUI.Dtos.ServiceDto;
 using Project.WebUI.Dtos.StaffDto;
 using Project.WebUI.Dtos.TestimonialDto;
@@ -29,6 +30,8 @@ namespace Project.WebUI.Helpers.Mapper
 
             CreateMap<ResultAboutUsDto, AboutUs>().ReverseMap();
             CreateMap<UpdateAboutUsDto, AboutUs>().ReverseMap();
+           
+            CreateMap<ResultRoomDto, Room>().ReverseMap();
         }
     }
 }
--- Frontend/Project.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ Frontend/Project.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,9 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project.WebUI.Dtos.AboutUsDto;
+using Project.WebUI.Dtos.RoomDto;
 
 namespace Project.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMsg = await client.GetAsync("http://localhost:5292/api/Room");
+
+            if (responseMsg.IsSuccessStatusCode)
+            {
+                var jsData = await responseMsg.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
