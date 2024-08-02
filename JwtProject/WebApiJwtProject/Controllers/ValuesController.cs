d07ca5575df382f49709db4afdd0900dfddd252f
Shenol <shenol88@hotmail.com>
2024-08-02 22:15:34 +0200

    jwt test

---
+++ JwtProject/WebApiJwtProject/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwtProject.Models;
+
+namespace WebApiJwtProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public  IActionResult Test()
+        {
+            return Ok(new CreateToken().TokenGenerate());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Test2()
+        {
+            return Ok("Welcome");
+        }
+    }
+}
---
+++ JwtProject/WebApiJwtProject/Models/CreateToken.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebApiJwtProject.Models
+{
+    public class CreateToken
+    {
+        public string TokenGenerate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("2024JwtProject-For_Learning_Purpose");
+            SymmetricSecurityKey symmetricSecurity = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(symmetricSecurity, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: "http://localhost",
+                audience: "http://localhost",
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(3),
+                signingCredentials: credentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
