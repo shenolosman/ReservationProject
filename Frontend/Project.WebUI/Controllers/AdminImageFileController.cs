b59c0b8ac5a85069568bccf27ab29708c5062506
Shenol <shenol88@hotmail.com>
2024-08-11 14:41:32 +0200

    upload file and image

---
+++ Frontend/Project.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Project.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public AdminImageFileController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var client = _httpClient.CreateClient();
+            await client.PostAsync("http://localhost:5192/api/FileImage", multipartFormDataContent);
+
+            return View();
+        }
+    }
+}
