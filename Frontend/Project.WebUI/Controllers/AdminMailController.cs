09a1cab3b09ee2dc4e0f70c0617338ec8c1ce0fe
Shenol <shenol88@hotmail.com>
2024-08-11 14:27:20 +0200

    mail sending from admin side

---
+++ Frontend/Project.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Project.WebUI.Models.Mail;
+
+namespace Project.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage(); 
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelletAdmin", "[gmail adress]");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Content;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            await client.ConnectAsync("smtp.gmail.com", 587, false);
+            await client.AuthenticateAsync("[gmail adress]", "[Password key from google]"); 
+            await client.SendAsync(mimeMessage);
+            await client.DisconnectAsync(true);
+
+
+            return View();
+        }
+    }
+}
---
+++ Frontend/Project.WebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.WebUI.Models.Mail
+{
+    public class AdminMailViewModel
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
