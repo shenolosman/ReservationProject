a9b908cf0f1c8b60b05f50ef913ba95213092fcd
Shenol <shenol88@hotmail.com>
2024-08-15 23:18:50 +0200

    external instagram api call for dashboard

---
+++ Frontend/Project.WebUI/Dtos/DashboardDto/FollowerDto/ResultInstagramDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Project.WebUI.Dtos.DashboardDto.FollowerDto
+{
+    public class ResultInstagramDto
+    {
+        public DataDto Data { get; set; }
+
+        public class DataDto
+        {
+            public int follower_count { get; set; }
+            public int following_count { get; set; }
+        }
+
+    }
+}
--- Frontend/Project.WebUI/ViewComponents/Dashboard/_DashboardSocialCountPartial.cs
+++ Frontend/Project.WebUI/ViewComponents/Dashboard/_DashboardSocialCountPartial.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Project.WebUI.Dtos.DashboardDto.FollowerDto;
 
 namespace Project.WebUI.ViewComponents.Dashboard
 {
@@ -12,6 +14,24 @@ namespace Project.WebUI.ViewComponents.Dashboard
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url=o.shenol"),
+                Headers =
+    {
+        { "x-rapidapi-key", "d7b9e0444cmsheedc6631451618ep1c1136jsn991c4383e79e" },
+        { "x-rapidapi-host", "instagram-scraper-api2.p.rapidapi.com" },
+    },
+            };
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            ResultInstagramDto resultInstagramDtos = JsonConvert.DeserializeObject<ResultInstagramDto>(body);
+            ViewBag.InstagramFollower = resultInstagramDtos.Data.follower_count;
+            ViewBag.InstagramFollowing = resultInstagramDtos.Data.following_count;
+
             return View();
         }
     }
